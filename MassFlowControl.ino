111699c770bf0d69d1acbb58390c7fe75e33bc37
st184918 <st184918@stud.uni-stuttgart.de>
2025-07-01 12:06:40 +0100

    Update MassFlowControl.ino

--- MassFlowControl.ino
+++ MassFlowControl.ino
@@ -4,16 +4,13 @@
 #define PWM_PIN 9 //can only use pin 9 or 10 for timer1 based PWM
 #define MFC_READOUT A0;
 
-int set_flow_rate = 500; // desired flow rate in SCCM
+unsigned int set_flow_rate = 500; // desired flow rate in SCCM
+unsigned int serial_flow_rate;
 
 void setup() {
 Serial.begin(9600);
 pinMode(PWM_PIN,OUTPUT); 
 Timer1.initialize(100); //100us = 10kHz (10x the MFC sampling rate)
-
-float duty_cycle = set_flow_rate * (1.023); // convert flow rate to duty cycle
-duty_cycle = round(duty_cycle); // round up for better replication of desired flow rate
-Timer1.pwm(PWM_PIN,duty_cycle); 
 
 // tell MATLAB that Arduino is ready to communicate
 Serial.println("Arduino is ready");
@@ -22,9 +19,15 @@ Serial.println("Arduino is ready");
 void loop() {
   // read flow rate from MATLAB
   if (Serial.available() > 0) {
-    unsigned int serial_flow_rate = Serial.read();
+    serial_flow_rate = Serial.read();
     Serial.write(serial_flow_rate);
   }
+
+  
+float duty_cycle = set_flow_rate * (1.023); // convert flow rate to duty cycle
+unsigned int duty_cycle_round = round(duty_cycle); // round up for better replication of desired flow rate
+Serial.println(duty_cycle_round);
+Timer1.pwm(PWM_PIN,duty_cycle_round); 
   
   // read back voltage from analog out and transform into a flow rate
 // int MFC_voltage_read = analogRead(MFC_READOUT);
